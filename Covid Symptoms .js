6db08f0ee473255b0192876b879ec5c32b67bb4b
Producer Intelligence <119265684+ProducerFeelings@users.noreply.github.com>
2023-12-03 19:33:25 -0800

    Covid Symptoms Page, About Page, and Faq Page
    I updated the about page, and the faq page, and the covid symptoms page.

--- Covid Symptoms .js
+++ Covid Symptoms .js
@@ -1,9 +1,7 @@
-window.onload = function() {
-    function changeBackgroundColor() {
-        // Set the background color to green
-        document.body.style.backgroundColor = 'green';
-    }
-
-    // Change the background color every 5 seconds
-    setInterval(changeBackgroundColor, 5000);
-};
+// JavaScript to change background color every 30 seconds
+setInterval(function() {
+    document.body.style.backgroundColor = "green"; // Change to desired color
+    setTimeout(function() {
+        document.body.style.backgroundColor = "pink"; // Change to initial color after 30 seconds
+    }, 15000); // Wait for 15 seconds to revert to the initial color
+}, 30000); // Repeat every 30 seconds
